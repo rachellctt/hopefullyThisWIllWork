22a8b498f6cabb299c979715de13439b65cb7cbc
Rachel <rachellctt@gmail.com>
2020-07-14 13:52:39 -0600

    Initial Commit

---
+++ hopefullyThisWIllWork/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  hopefullyThisWIllWork
+//
+//  Created by Rachel Tat on 7/14/20.
+//  Copyright © 2020 Rachel Tat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ hopefullyThisWIllWorkTests/hopefullyThisWIllWorkTests.swift
@@ -0,0 +1,34 @@
+//
+//  hopefullyThisWIllWorkTests.swift
+//  hopefullyThisWIllWorkTests
+//
+//  Created by Rachel Tat on 7/14/20.
+//  Copyright © 2020 Rachel Tat. All rights reserved.
+//
+
+import XCTest
+@testable import hopefullyThisWIllWork
+
+class hopefullyThisWIllWorkTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
